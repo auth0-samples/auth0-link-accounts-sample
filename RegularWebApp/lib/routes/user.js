9eb56a2a0c2542517d9009298719cd602389929c
zulfiqar <zulfiqar@auth0.com>
2020-04-25 13:20:09 +0500

    Rewrite: - Removed lock and changed to use universal login - Removed passport-js, added express-oidc-connect - Improved security by enforcing and validating the login of target account

--- RegularWebApp/lib/routes/user.js
+++ RegularWebApp/lib/routes/user.js
@@ -1,36 +1,70 @@
-'use strict';
+"use strict";
 
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn('/');
-const Auth0Client = require('../Auth0Client');
-const express = require('express');
-const router = express.Router();
-const _ = require('lodash');
+const debug = require("debug")("auth0-link-accounts-sample");
 
-const ManagementClient = require('auth0').ManagementClient
-const management = new ManagementClient({
-  token: process.env.AUTH0_APIV2_TOKEN,
-  domain: process.env.AUTH0_DOMAIN
-});
+const router = require("express").Router();
+const auth0Client = require("../Auth0Client");
+const _ = require("lodash");
+const { Errors, clear } = require("../flashErrors");
+
+// const ManagementClient = require("auth0").ManagementClient;
+// const management = new ManagementClient({
+//   token: process.env.AUTH0_APIV2_TOKEN,
+//   domain: process.env.AUTH0_DOMAIN,
+// });
+
+// const router = auth({
+//   required: false,
+//   appSession: false,
+//   handleCallback: (req, res, next) => {
+//     console.log(req.openidTokens);
+//     console.log(req.openidState);
+//     user = req.openidTokens.claims();
+//     next();
+//   },
+//   // getLoginState: (req, config) => {
+//   //   console.log(config);
+//   //   return {
+//   //     originalUser: "xxxxx",
+//   //     online: false,
+//   //   };
+//   // },
+// });
 
 /*
-* Recursively merges user_metadata and app_metadata from secondary into primary user.
-* Data of primary user takes preponderance.
-* Array fields are joined.
-*/
-function _mergeMetadata(primaryUser, secondaryUser){
-  
-  const customizerCallback = function(objectValue, sourceValue){
-    if (_.isArray(objectValue)){
+ * Recursively merges user_metadata and app_metadata from secondary into primary user.
+ * Data of primary user takes preponderance.
+ * Array fields are joined.
+ */
+function _mergeMetadata(primaryUser, secondaryUser) {
+  const customizerCallback = function (objectValue, sourceValue) {
+    if (_.isArray(objectValue)) {
       return sourceValue.concat(objectValue);
     }
   };
-  const mergedUserMetadata = _.merge({}, secondaryUser.user_metadata, primaryUser.user_metadata, customizerCallback);
-  const mergedAppMetadata = _.merge({}, secondaryUser.app_metadata, primaryUser.app_metadata, customizerCallback);
-  
+  const mergedUserMetadata = _.merge(
+    {},
+    secondaryUser.user_metadata,
+    primaryUser.user_metadata,
+    customizerCallback
+  );
+  const mergedAppMetadata = _.merge(
+    {},
+    secondaryUser.app_metadata,
+    primaryUser.app_metadata,
+    customizerCallback
+  );
+
   return Promise.all([
-    management.users.updateUserMetadata({ id: primaryUser.sub }, mergedUserMetadata),
-    management.users.updateAppMetadata({ id: primaryUser.sub }, mergedAppMetadata)
-  ]).then(result => {
+    management.users.updateUserMetadata(
+      { id: primaryUser.sub },
+      mergedUserMetadata
+    ),
+    management.users.updateAppMetadata(
+      { id: primaryUser.sub },
+      mergedAppMetadata
+    ),
+  ]).then((result) => {
     //save result in primary in session
     primaryUser.user_metadata = result[0].user_metadata;
     primaryUser.app_metadata = result[1].app_metadata;
@@ -38,68 +72,84 @@ function _mergeMetadata(primaryUser, secondaryUser){
 }
 
 /* GET user profile. */
-router.get('/', ensureLoggedIn, function(req, res) {
-  console.log('user', req.user);
-  
-  Auth0Client.getUser(req.user._json.sub)
-    .then(user =>  {
-      console.log('returning user', user);
-      res.render('user', { 
-        user: user, //returning req.user._json, since it contains the user_metadata and app_metadata properties the root user doesn't have.
-          })
-    })
-});
-
-router.get('/suggested-users',ensureLoggedIn, (req,res) => {
-  let suggestedUsers = [];
-  
-  Auth0Client.getUsersWithSameVerifiedEmail(req.user._json)
-    .then(identities => {
-      suggestedUsers = identities;
-    }).catch( err => {
-      console.log('There was an error retrieving users with the same verified email to suggest linking',err);
-    }).then(() => {
-      res.send(suggestedUsers);
+router.get("/", async (req, res) => {
+  const user = req.openid.user;
+  //fetch user profile containing the user_metadata and app_metadata properties
+  try {
+    const profile = await auth0Client.getUser(user.sub);
+    const flashError = clear(req);
+    res.render("user", {
+      user: profile,
+      wrongAccountError: flashError && flashError === Errors.WrongAccount,
     });
+  } catch (err) {
+    debug("GET /user failed: %o", err);
+    res.render("error", err);
+  }
 });
 
-router.post('/link-accounts/:targetUserId', ensureLoggedIn, (req,res,next) => {
-    // Fetch target user to make verifications and merge metadata
-  
-    Auth0Client.getUser(req.params.targetUserId)
-    .then( targetUser => {
+router.get("/suggested-users", async (req, res) => {
+  debug("GET /suggested-users ...");
+  try {
+    const suggestedUsers = await auth0Client.getUsersWithSameVerifiedEmail(
+      req.openid.user
+    );
+    res.send(suggestedUsers);
+  } catch (err) {
+    debug("GET /user/suggested-users failed: %o", err.message);
+    res.send([]);
+  }
+});
 
+router.post("/link-accounts/:targetUserId", (req, res, next) => {
+  // Fetch target user to make verifications and merge metadata
+
+  Auth0Client.getUser(req.params.targetUserId)
+    .then((targetUser) => {
       // verify email (this is needed because targetUserId came from client side)
-      if(! targetUser.email_verified || targetUser.email !== req.user._json.email){
-        throw new Error('User not valid for linking');
+      if (
+        !targetUser.email_verified ||
+        targetUser.email !== req.user._json.email
+      ) {
+        throw new Error("User not valid for linking");
       }
-  
+
       //merge metadatax
       return _mergeMetadata(req.user._json, targetUser);
     })
     .then(() => {
-      return Auth0Client.linkAccounts(req.user._json.sub, req.params.targetUserId);
+      return Auth0Client.linkAccounts(
+        req.user._json.sub,
+        req.params.targetUserId
+      );
     })
-    .then( identities => {
+    .then((identities) => {
       req.user.identities = req.user._json.identities = identities;
       res.send(identities);
     })
-    .catch( err => {
-      console.log('Error linking accounts!',err);
+    .catch((err) => {
+      console.log("Error linking accounts!", err);
       next(err);
     });
 });
 
-router.post('/unlink-accounts/:targetUserProvider/:targetUserId',ensureLoggedIn, (req,res,next) => {
-  Auth0Client.unlinkAccounts(req.user._json.sub, req.params.targetUserProvider, req.params.targetUserId)
-  .then( identities => {
-    req.user.identities = req.user._json.identities = identities;
-    res.send(identities);
-  })
-  .catch( err => {
-    console.log('Error unlinking accounts!',err);
-    next(err);
-  });
-});
+router.post(
+  "/unlink-accounts/:targetUserProvider/:targetUserId",
+  (req, res, next) => {
+    Auth0Client.unlinkAccounts(
+      req.user._json.sub,
+      req.params.targetUserProvider,
+      req.params.targetUserId
+    )
+      .then((identities) => {
+        req.user.identities = req.user._json.identities = identities;
+        res.send(identities);
+      })
+      .catch((err) => {
+        console.log("Error unlinking accounts!", err);
+        next(err);
+      });
+  }
+);
 
 module.exports = router;
